b7b32c7279cdcc1626220e4392ab528a1fa9727a
kaarejoergensen <kaare1994@gmail.com>
2018-01-04 16:31:54 +0100

    Added inspiration-snake

---
+++ src/main/java/org/team08/pspacessnake/GUI/Snake.java
@@ -0,0 +1,31 @@
+package org.team08.pspacessnake.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new Snake();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
