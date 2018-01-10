430354e44062d4493c39f02e05b46ee67c05a993
Frederik Kirkegaard <frederik-kkirkegaard@hotmail.com>
2018-01-10 10:25:48 +0100

    Change position of GUI.

--- src/main/java/org/team08/pspacessnake/Client/Client.java
+++ src/main/java/org/team08/pspacessnake/Client/Client.java
@@ -8,6 +8,11 @@ import org.team08.pspacessnake.Model.Room;
 import org.team08.pspacessnake.Model.Token;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import org.team08.pspacessnake.GUI.*;
 import org.team08.pspacessnake.GUI.logic.Point;
@@ -16,20 +21,22 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
+@SuppressWarnings("restriction")
 public class Client {
     private final static String REMOTE_URI = "tcp://127.0.0.1:9001/";
-
+    private static Token token;
+    private static Space space;
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter name: ");
         String name = scanner.nextLine();
 
-        Space space = new RemoteSpace(REMOTE_URI + "space?keep");
+        space = new RemoteSpace(REMOTE_URI + "space?keep");
 
         space.put("createClient", name);
         Object[] createClientResult = space.get(new ActualField("createClientResult"), new ActualField(name), new
                 FormalField(Token.class));
-        Token token = (Token) createClientResult[2];
+        token = (Token) createClientResult[2];
 
         System.out.println("1: Create new room");
         List<Object[]> rooms = space.queryAll(new ActualField("room"), new FormalField(String.class), new FormalField
@@ -77,8 +84,37 @@ public class Client {
 ////        Object[] startPoints = space.queryp(new ActualField("Starting position"), new FormalField(List.class));
 ////        gui.startingPositions((List<Point>) startPoints[1]);
 
-        Application.launch(SpaceGui.class, args);
+        Application.launch(args);
     }
+
+    public void start(Stage primaryStage) throws Exception {
+		SpaceGui gui = new SpaceGui(token, primaryStage);
+		new Thread(new GameReader(space, gui)).start();
+    }
+    
+}
+class GameReader implements Runnable {
+	private Space space;
+	private SpaceGui gui;
+
+	public GameReader(Space space, SpaceGui gui) {
+		this.space = space;
+		this.gui = gui;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Object[] newPoint = space.get(new ActualField("Player moved"), new FormalField(String.class), 
+						new FormalField(Point.class));
+
+				gui.updateGui((Point) newPoint[2]); 
+
+			} catch (InterruptedException e) {}
+		}
+
+	}
 }
 
 class Reader implements Runnable {
--- src/main/java/org/team08/pspacessnake/GUI/SpaceGui.java
+++ src/main/java/org/team08/pspacessnake/GUI/SpaceGui.java
@@ -14,6 +14,7 @@ import org.jspace.Space;
 
 import org.team08.pspacessnake.GUI.logic.Point;
 import org.team08.pspacessnake.GUI.logic.Snake;
+import org.team08.pspacessnake.Model.Token;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -25,7 +26,7 @@ import javafx.stage.Stage;
 
 
 @SuppressWarnings("restriction")
-public class SpaceGui extends Application {
+public class SpaceGui {
 	private Space space;
 	private int rows;
 	private int cols;
@@ -33,11 +34,11 @@ public class SpaceGui extends Application {
 	private final static String REMOTE_URI = "tcp://127.0.0.1:9001/";
 	public static final int WIDTH = 1000;
 	public static final int HEIGHT = 1000;
-	private static GraphicsContext context;
 	private boolean keyPressed = false;
 	private List<Point> points;
+	private static GraphicsContext context;
 	
-	public void start(Stage primaryStage) throws Exception {
+	public SpaceGui(Token token, Stage primaryStage) throws UnknownHostException, IOException {
 		points = new LinkedList<>();
 		StackPane root = new StackPane();
 		Canvas canvas = new Canvas(WIDTH, HEIGHT);
@@ -107,7 +108,6 @@ public class SpaceGui extends Application {
 		primaryStage.show();
 
 
-		new Thread(new UpdateGuiThread(space,this)).start();
 
 	}
 
@@ -148,26 +148,4 @@ public class SpaceGui extends Application {
 	}
 }
 
-class UpdateGuiThread implements Runnable {
-	private Space space;
-	private SpaceGui gui;
 
-	public UpdateGuiThread(Space space, SpaceGui gui) {
-		this.space = space;
-		this.gui = gui;
-	}
-
-	@Override
-	public void run() {
-		while (true) {
-			try {
-				Object[] newPoint = space.get(new ActualField("Player moved"), new FormalField(String.class), 
-						new FormalField(Point.class));
-
-				gui.updateGui((Point) newPoint[2]); 
-
-			} catch (InterruptedException e) {}
-		}
-
-	}
-}
