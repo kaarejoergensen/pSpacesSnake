fd6b3918c37449da46e202ee748fb035c4ccc54e
Simon Freiesleben <s.freiesleben.jensen@gmail.com>
2018-01-11 12:30:20 +0100

    temp

--- src/main/java/org/team08/pspacessnake/GUI/floatTests.java
+++ src/main/java/org/team08/pspacessnake/GUI/floatTests.java
@@ -6,20 +6,21 @@ import java.util.ListIterator;
 import java.util.Iterator;
 
 public class floatTests {
+
+	// Size of game Board in pixels
+    private static final int B_HEIGHT = 600;
+    private static final int B_WIDTH = 800;
+    private static final int BIN_SIZE = 5;
+
+    // Cells of 10x10 pixels
+    static int numRow = (int)Math.floor(B_HEIGHT / (double) BIN_SIZE) + 1;
+    static int numCol = (int)Math.floor(B_WIDTH / (double) BIN_SIZE) + 1;
+    static LinkedList<Point2D.Double>[][] boardCells = (LinkedList<Point2D.Double>[][]) new LinkedList<?>[numRow][numCol];
+    static ListIterator<Point2D.Double>[][] boardCellsIterators = (ListIterator<Point2D.Double>[][]) new ListIterator<?>[numRow][numCol]; 
+    
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		
-		// Size og game Board in pixels
-	    final int B_HEIGHT = 600;
-	    int B_WIDTH = 800;
-	    final int BIN_SIZE = 5;
-	    
-	    // Cells of 10x10 pixels
-	    int numRow = (int)Math.floor(B_HEIGHT / (double) BIN_SIZE) + 1;
-	    int numCol = (int)Math.floor(B_WIDTH / (double) BIN_SIZE) + 1;
-	    LinkedList<Point2D.Double>[][] boardCells = (LinkedList<Point2D.Double>[][]) new LinkedList<?>[numRow][numCol];
-	    ListIterator<Point2D.Double>[][] boardCellsIterators = (ListIterator<Point2D.Double>[][]) new ListIterator<?>[numRow][numCol]; 
-	    
+		    
 	    // Initialiste 2D arrays
 	    for (int row = 0; row < numRow; row++) {
 	    	for (int col = 0; col < numCol; col++) {
@@ -28,14 +29,16 @@ public class floatTests {
 	    	}
 	    }
 	    
-	    
-
-	    
+	      
 	    // Random points for testing
-	    Point2D.Double somePos = new Point2D.Double(3.4d, 5.1d);
-	    Point2D.Double someOtherPos = new Point2D.Double(0.2d, 2.3d);
+	    Point2D.Double somePos = new Point2D.Double(0.0, 0.0);
+	    Point2D.Double someOtherPos = new Point2D.Double(3.0, 3.0);
+	    addPoint(somePos);
+	    //addPoint(someOtherPos);
 	    
-	    // test 
+	    System.out.println(checkCollision(someOtherPos));
+	    
+	    /*// test 
 	    boardCellsIterators[6][8].add(somePos);
 	    boardCellsIterators[6][8].add(someOtherPos);
 	    boardCellsIterators[6][8] = boardCells[6][8].listIterator(); //reset ListIterator
@@ -43,12 +46,43 @@ public class floatTests {
 	    System.out.println(boardCellsIterators[6][8].next());
 	    System.out.println(boardCellsIterators[6][8].previous());
 	    System.out.println(boardCellsIterators[6][8].previous());
-	
+	    */
+	    
+	    
+	    
+	    	
 	}
 	
-    public void savePoint(Point2D.Double point) {
+	public static boolean isOnBoard(Point2D.Double point) {
+		return (point.getX() < 0d && point.getY() > (double) B_WIDTH && point.getY() < 0d && point.getY() > B_HEIGHT);
+	}
+	
+	public static boolean checkCollision(Point2D.Double point) {
+		int pointCellX = (int) point.getX(); //the x coordinate of the bin the point belongs to.
+		int pointCellY = (int) point.getY(); //the y coordinate of the bin the point belongs to.
+		
+		Iterator tempIterator;
+		for (int dx = -1; dx <= 1; dx++) {
+			if (!(pointCellX + dx >= 0 && pointCellX + dx <= numRow))
+				continue;
+			for (int dy = -1; dy <= 1; dy++) {
+				if (!(pointCellY + dy >= 0 && pointCellY + dy <= numCol))
+					continue;
+				tempIterator = boardCells[pointCellY + dy][pointCellX + dx].listIterator();
+				while (tempIterator.hasNext()) {
+					if (point.distance((Point2D.Double)tempIterator.next()) < 5d)
+						return true;
+				}
+					
+			}
+		}
+		return false;
+	}
+	
+    public static void addPoint(Point2D.Double point) {
     	int i = (int) point.getX() / BIN_SIZE;
     	int j = (int) point.getY() / BIN_SIZE;
     	boardCellsIterators[i][j].add(point);
     }
+    
 }
