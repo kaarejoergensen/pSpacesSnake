bfbf14181ab74565583ba787e361e9fea7e59943
Simon Freiesleben <s.freiesleben.jensen@gmail.com>
2018-01-11 10:52:19 +0100

    added GameControl

---
+++ src/main/java/org/team08/pspacessnake/Server/GameControl.java
@@ -0,0 +1,10 @@
+package org.team08.pspacessnake.Server;
+
+import org.jspace.*;
+
+public class GameControl {
+	//private final int NUM_OF_PLAYERS;
+	
+	//public GameControl(int numberOfPlayers)
+
+}
