97c87f94ec60bc2701686f50f6cb6f1b70e306f2
Simon Freiesleben <s.freiesleben.jensen@gmail.com>
2018-01-18 09:53:57 +0100

    rettet player

--- src/main/java/org/team08/pspacessnake/Model/Player.java
+++ src/main/java/org/team08/pspacessnake/Model/Player.java
@@ -3,110 +3,93 @@ package org.team08.pspacessnake.Model;
 import java.util.LinkedList;
 
 public class Player {
-<<<<<<< HEAD
-        private Token token;
-        private Point position;
-        private LinkedList<Point> pointBuffer = new LinkedList<Point>();
-        private Double speed;
-        private double size;		// diameter of snake head.
-        private Double angle;		//current angler of movement
-        private double dAngle;		//the change of angle per frame.
-        private String direction;    //change of angle
-        private boolean isDead = false;
-        private boolean remember = true;
-        private boolean ready;
-        private boolean edgeJumper;
-        private boolean coarseTurner;
-=======
-    private Token token;
-    private Point position;
-    private LinkedList<Point> pointBuffer = new LinkedList<Point>();
-    private Double speed;
-    private double size;		// diameter of snake head. 
-    private Double angle;		//current angler of movement
-    private double dAngle;		//the change of angle per frame.
-    private String direction;    //change of angle
-    private boolean isDead = false;
-    private boolean remember = true;
-    private boolean ready;
-    private boolean edgeJumper;
-    private boolean coarseTurner;
-    private PowerUps power;
->>>>>>> branch 'master' of https://github.com/kaarejoergensen/pSpacesSnake.git
+	private Token token;
+	private Point position;
+	private LinkedList<Point> pointBuffer = new LinkedList<Point>();
+	private Double speed;
+	private double size;		// diameter of snake head.
+	private Double angle;		//current angler of movement
+	private double dAngle;		//the change of angle per frame.
+	private String direction;    //change of angle
+	private boolean isDead = false;
+	private boolean remember = true;
+	private boolean ready;
+	private boolean edgeJumper;
+	private boolean coarseTurner;
 
-    public Player(Token token) {
-        this.setToken(token);
-        this.setSpeed(3d);
-        this.setSize(5d);
-        this.setAngle(0d);
-        this.setDAngle(.17);
-        this.setDirection("none");
-        this.setEdgeJumper(false);
-        this.setCoarseTurner(false);
-    }
+	public Player(Token token) {
+		this.setToken(token);
+		this.setSpeed(3d);
+		this.setSize(5d);
+		this.setAngle(0d);
+		this.setDAngle(.17);
+		this.setDirection("none");
+		this.setEdgeJumper(false);
+		this.setCoarseTurner(false);
+	}
 
-    public Point move(int boardWidth, int boardHeight) {
-        double angle = this.isCoarseTurner() ? coarseAngle(this.getAngle()) : this.getAngle();
-    	if (this.isEdgeJumper())
-   	        this.position = this.position.translate(speed * Math.cos(angle), -speed * Math.sin(angle), this.getPosition().getRadius(), this.position.getColor(), boardWidth, boardHeight);
-    	else
-            this.position = this.position.translate(speed * Math.cos(angle), -speed * Math.sin(angle), this.getPosition().getRadius(), this.position.getColor());
-    	return this.position;
-    }
+	public Point move(int boardWidth, int boardHeight) {
+		double angle = this.isCoarseTurner() ? coarseAngle(this.getAngle()) : this.getAngle();
+		if (this.isEdgeJumper())
+			this.position = this.position.translate(speed * Math.cos(angle), -speed * Math.sin(angle), this.getPosition().getRadius(), this.position.getColor(), boardWidth, boardHeight);
+		else
+			this.position = this.position.translate(speed * Math.cos(angle), -speed * Math.sin(angle), this.getPosition().getRadius(), this.position.getColor());
+		return this.position;
+	}
 
 
 
-    public double coarseAngle(double angle) {
-        angle = reduceAngleToPrimaryInterval(angle);
-        angle -= this.getCoarseStartAngle();
-        angle /= Math.PI/2; // scale to interval [0;4[
-        angle = Math.round(angle);
-        angle *= Math.PI/2;
-        angle += this.getCoarseStartAngle();
-        return angle;
-    }
+	public double coarseAngle(double angle) {
+		angle = reduceAngleToPrimaryInterval(angle);
+		angle -= this.getCoarseStartAngle();
+		angle /= Math.PI/2; // scale to interval [0;4[
+		angle = Math.round(angle);
+		angle *= Math.PI/2;
+		angle += this.getCoarseStartAngle();
+		return angle;
+	}
 
-    public static double reduceAngleToPrimaryInterval(double angle) {
-        while (angle < 0){  // make sure the angle i positive to prevent odd modulo operation behaviour.
-            angle += 2*Math.PI;
-        }
-        return angle % (Math.PI * 2);
-    }
+	public static double reduceAngleToPrimaryInterval(double angle) {
+		while (angle < 0){  // make sure the angle i positive to prevent odd modulo operation behaviour.
+			angle += 2*Math.PI;
+		}
+		return angle % (Math.PI * 2);
+	}
 
-    public double turn() {
-        if (!this.direction.equals("none")) {
-            if (this.direction.equals("left")) {
-                this.angle += this.dAngle;
-            } else if (direction.equals("right")) {
-                this.angle -= this.dAngle;
-            }
-        }
-        return this.angle;
-    }
+	public double turn() {
+		if (!this.direction.equals("none")) {
+			if (this.direction.equals("left")) {
+				this.angle += this.dAngle;
+			} else if (direction.equals("right")) {
+				this.angle -= this.dAngle;
+			}
+		}
+		return this.angle;
+	}
 
-    public Token getToken() {
-        return token;
-    }
+	public Token getToken() {
+		return token;
+	}
 
-    public void setToken(Token token) {
-        this.token = token;
-    }
+	public void setToken(Token token) {
+		this.token = token;
+	}
 
-    public Point getPosition() {
-        return position;
-    }
+	public Point getPosition() {
+		return position;
+	}
 
-    public void setPosition(Point position) {
-        this.position = position;
-    }
+	public void setPosition(Point position) {
+		this.position = position;
+	}
 
-    public LinkedList<Point> getPointBuffer() {
+	public LinkedList<Point> getPointBuffer() {
 		return pointBuffer;
 	}
 
 	public Double getSpeed() {
-        return speed;
-    }
+		return speed;
+	}
 
 	public double getSize() {
 		return size;
@@ -117,56 +100,56 @@ public class Player {
 	}
 
 	public void setSpeed(Double speed) {
-        this.speed = speed;
+		this.speed = speed;
 	}
-    
-    public Double getAngle() {
-        return angle;
-    }
 
-    public void setAngle(Double angle) {
-        this.angle = angle;
-    }
+	public Double getAngle() {
+		return angle;
+	}
 
-    public double getDAngle() {
-        return dAngle;
-    }
+	public void setAngle(Double angle) {
+		this.angle = angle;
+	}
 
-    public void setDAngle(double dAngle) {
-        this.dAngle = dAngle;
-    }
+	public double getDAngle() {
+		return dAngle;
+	}
 
-    public String getDirection() {
-        return direction;
-    }
+	public void setDAngle(double dAngle) {
+		this.dAngle = dAngle;
+	}
 
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
+	public String getDirection() {
+		return direction;
+	}
 
-    public boolean isDead() {
-        return isDead;
-    }
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
 
-    public void kill() {
-        this.isDead = true;
-    }
+	public boolean isDead() {
+		return isDead;
+	}
 
-    public void setRemember(boolean holes) {
-        this.remember = holes;
-    }
+	public void kill() {
+		this.isDead = true;
+	}
 
-    public boolean getRemember() {
-        return remember;
-    }
+	public void setRemember(boolean holes) {
+		this.remember = holes;
+	}
 
-    public boolean isReady() {
-        return ready;
-    }
+	public boolean getRemember() {
+		return remember;
+	}
 
-    public void setReady(boolean ready) {
-        this.ready = ready;
-    }
+	public boolean isReady() {
+		return ready;
+	}
+
+	public void setReady(boolean ready) {
+		this.ready = ready;
+	}
 
 	public boolean isEdgeJumper() {
 		return edgeJumper;
@@ -176,28 +159,20 @@ public class Player {
 		this.edgeJumper = edgeJumper;
 	}
 
-    public boolean isCoarseTurner() {
-        return coarseTurner;
-    }
+	public boolean isCoarseTurner() {
+		return coarseTurner;
+	}
 
-    public void setCoarseTurner(boolean coarseTurner) {
-        this.coarseTurner = coarseTurner;
-    }
-    public double getCoarseStartAngle() {
-        return coarseStartAngle;
-    }
+	public void setCoarseTurner(boolean coarseTurner) {
+		this.coarseTurner = coarseTurner;
+	}
+	public double getCoarseStartAngle() {
+		return coarseStartAngle;
+	}
 
-    public void setCoarseStartAngle(double coarseStartAngle) {
-        this.coarseStartAngle = coarseStartAngle;
-    }
+	public void setCoarseStartAngle(double coarseStartAngle) {
+		this.coarseStartAngle = coarseStartAngle;
+	}
 
-    private double coarseStartAngle;
-
-    public PowerUps getPower() {
-        return power;
-    }
-
-    public void setPower(PowerUps power) {
-        this.power = power;
-    }
+	private double coarseStartAngle;
 }
